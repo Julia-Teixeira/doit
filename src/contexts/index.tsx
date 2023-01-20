058e271d43e6fba200dc62a8e27c06671594a98f
Julia Pereira <juliapt95@gmail.com>
2023-01-20 19:43:53 -0300

    feat: Auth Login

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { theme } from "../styles/theme";
+import { AuthProvider } from "./authContext";
 
 interface iAppProviderProps {
   children: ReactNode;
 }
 export const AppProvider = ({ children }: iAppProviderProps) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  <AuthProvider>
+    <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  </AuthProvider>
 );
